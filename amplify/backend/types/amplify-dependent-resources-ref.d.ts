8dc21fe14775070cc53d6c398919c1cd1167065c
Vivek Rao <vivekrao686@gmail.com>
2026-02-26 15:24:30 +0530

    initialized AWS Amplify project and deployed Amazon Cognito authentication using Amplify CLI (Gen 1)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyauthapp1ee6923a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
